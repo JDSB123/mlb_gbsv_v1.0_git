0cbdf970894917850ca842e3374480b0cdda9955
JDSB <jdsb@bierfam.com>
2026-03-22 20:52:49 +0100

    chore: remove staging patchfiles

--- patch_toml2.py
+++
@@ -1,7 +0,0 @@
-with open('pyproject.toml', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-text = text.replace('warn_return_any = true', 'warn_return_any = false')
-
-with open('pyproject.toml', 'w', encoding='utf-8') as f:
-    f.write(text)
